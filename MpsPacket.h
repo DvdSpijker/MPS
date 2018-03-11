eacc3a90ea5eba71a0e6c73d444a0838ba8aa93f
D. van de Spijker <spijker.dorus@gmail.com>
2018-03-11 01:05:40 +0100

    + MpsPacket : More comments.

--- MpsPacket.h
+++ MpsPacket.h
@@ -22,17 +22,17 @@ struct MpsPacketFieldList {
 
 struct MpsPacket {
     uint32_t id;
-	
 	MpsPacketSrc_t src; /* Packet source. */
 	MpsPacketDest_t dest; /* Packet destination. */
+	MpsPacketSize_t packet_size; /* Total packet size. */
 	
 	struct MpsPacketFieldList headers_list;
 	struct MpsPacketFieldList trailers_list;
 	
 	uint8_t *payload;
 	MpsPacketSize_t payload_size;
-	MpsPacketSize_t packet_size;
-    uint8_t type;		/* Event or Data type. See MPS_PACKET_TYPE_* below. */
+	
+    uint8_t type;		/* Event, Data or Cmd type. See MPS_PACKET_TYPE_* below. */
     void    *layer_specific; /* Optional pointer for layer specific data-structures. */
 
     struct MpsPacket *next_in_queue;	/* Used by MpsQueue for linking. */
@@ -44,17 +44,19 @@ struct MpsPacket {
  * to the top or bottom layer. */
 #define MPS_PACKET_TYPE_EVENT	0x01 
 
-/* A packet containing a packet can be modified by layers as it propagates
+/* A packet containing data can be modified by layers as it propagates
  * through the stack. Layers can add/remove sections of the packet e.g. a header. */
 #define MPS_PACKET_TYPE_DATA	0x02
 
-/* Creates a MspPacket of given size and sets the layer specific implementation. */
-MpsPacketHandle_t MpsPacketCreate(uint8_t *buffer, MpsPacketSize_t size, uint8_t type, void *layer_specific);
+/* A packet containing a packet can be modified by layers as it propagates
+ * through the stack. Layers can add/remove sections of the packet e.g. a header. */
+#define MPS_PACKET_TYPE_CMD		0x03
+
+/* Creates a MpsPacket of given size and sets the layer specific implementation. 
+ * */
+MpsPacketHandle_t MpsPacketCreate(uint8_t type, void *layer_specific);
 
 void MpsPacketDelete(MpsPacketHandle_t packet);
-
-/* Resizes the packet while retaining the data. */
-MpsPacketHandle_t MpsPacketResize(MpsPacketHandle_t packet, MpsPacketSize_t new_size);
 
 /* TODO: Returns a copy of the packet. */
 MpsPacketHandle_t MpsPacketCopy(MpsPacketHandle_t packet);
@@ -75,28 +77,22 @@ MpsPacketSize_t MpsPacketPayloadGet(MpsPacketHandle_t packet, uint8_t *data);
 /* Adds data to the header region after merging a possible previous header with the current body.
  * After the operation the header size will be the added header size.
  * Header and body offsets are adjusted. */
-MpsResult_t MpsPacketHeaderAdd(MpsPacketHandle_t packet, uint8_t *data, MpsPacketSize_t size);
+MpsResult_t MpsPacketHeaderAdd(MpsPacketHandle_t packet, uint8_t *data, uint8_t size);
 
 /* Adds data to the trailer region after merging a possible previous trailer with the current body.
  * After the operation the trailer size will be the added trailer size.
  * The trailer offset is adjusted. */
-MpsResult_t MpsPacketTrailerAdd(MpsPacketHandle_t packet, uint8_t *data, MpsPacketSize_t size);
+MpsResult_t MpsPacketTrailerAdd(MpsPacketHandle_t packet, uint8_t *data, uint8_t size);
 
 /* Removes a header from the packet. After the
  * operation the header region size will be the removed header size.
  * Header and body offsets are adjusted. */
 uint8_t MpsPacketHeaderRemove(MpsPacketHandle_t packet, uint8_t *data);
 
-/* Removes a body of defined size from the current body. After the
- * operation the body region size will decreased by the removed body size.
- * The body offset is adjusted.
- * Note: If 0 is passed as the size, the entire body will be removed. */
-MpsResult_t MpsPacketBodyRemove(MpsPacketHandle_t packet, uint8_t *data);
-
 /* Removes a trailer of defined size from the current body. After the
  * operation the trailer region size will be the removed trailer size.
  * The trailer offset is adjusted. */
-MpsResult_t MpsPacketTrailerRemove(MpsPacketHandle_t packet, uint8_t *data, MpsPacketSize_t size);
+uint8_t MpsPacketTrailerRemove(MpsPacketHandle_t packet, uint8_t *data, MpsPacketSize_t size);
 
 uint8_t MpsPacketTypeGet(MpsPacketHandle_t packet);
 
